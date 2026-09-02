c56749bf33d6e2ab1dd948a35ffa2d153359d8f7
Ivan Trunaev <trunaev.ivan.1987@gmail.com>
2026-09-03 00:31:06 +0500

    Buggy demo

--- player.pl
+++ player.pl
@@ -1,10 +1,14 @@
 #!/usr/bin/perl
 
-#use 5.022;
+use 5.022;
 use warnings;
 
 use Audio::PortAudio;
 use JSON::XS;
+
+use lib 'lib';
+use VectorTracer;
+
 
 # Init audio system
 my $api = Audio::PortAudio::default_host_api();
@@ -26,10 +30,34 @@ my $s = join '', @input;
 
 my $json = decode_json $s;
 
+my $length = $json->{data}->{length};
+my $f = $json->{data}->{f};
 
 
-#for (0 .. 400) {
-#    $stream->write($sine);
-#}
+my $tracer = new VectorTracer(debug => 0);
+my $node = $tracer->parse($f);
+
+my @s = ();
+for my $x (0 .. $length) {
+	# Render audio from formula
+	my $signal = $tracer->trace(undef, $x);
+	
+	# Limiter
+	$signal = 1 if ($signal > 1);
+	$signal = -1 if ($signal < -1);
+	push @s, $signal;
+}
+
+my $wave = pack "f*", @s;
+
+
+
+# Play
+for (0 .. 400) {
+	$stream->write($wave);
+}
+
+
+
 
 1;
